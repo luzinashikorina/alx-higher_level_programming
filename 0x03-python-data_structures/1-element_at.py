7a10a3edd1c52e4ac50640e00e4507424955dc7c
luzinashikorina <luzabiniam01@gmail.com>
2023-01-11 05:41:38 +0300

    Success 13&1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
-
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx > len(my_list) - 1:
+    if idx >= len(my_list):
         return None
-
-    item = my_list[idx]
-    return
+    else:
+        return my_list[idx]n
